e565a4c0c510d60b732c06c84a6544264036691d
Nicky Cadavillo <nicoleacadavillo@gmail.com>
2016-03-21 17:27:19 -0400

    array of todos added and ng repeat working

--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -1,10 +1,13 @@
 
 todoApp.controller('todoController', ['$scope', '$http', function($scope, $http){
   $scope.message = "hello world";
-  $scope.todo = function(){
-    
-  };
-
-
-
+  $scope.todos = [
+    {"todo": "finish taxes"},
+    {"todo": "go to the post office"},
+    {"todo": "prep lunch for tomorrow"},
+    {"todo": "go over euler questions"},
+    {"todo": "add angularjs routing to blog"},
+    {"todo": "book airbnb"},
+    {"todo": "book plane flights for may trip"}
+  ] //closes array
 }]); //closes todoApp controller
